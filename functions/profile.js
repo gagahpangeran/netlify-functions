240add3bdf0fcca2b6ca0a944a7e3c530cbb503c
Gagah Pangeran <gagahpangeran@gmail.com>
2020-03-04 20:41:01 +0700

    remove point

--- functions/profile.js
+++ functions/profile.js
@@ -11,7 +11,6 @@ exports.handler = async event => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
-  const point = Math.floor(getRandomInt(0, 10000) / 1000) * 1000;
   const referral = getRandomInt(5, 20);
   const interview = getRandomInt(3, referral);
   const placement = getRandomInt(0, interview);
@@ -19,7 +18,7 @@ exports.handler = async event => {
   const data =
     username === "" || username === undefined
       ? {}
-      : { username, point, referral, interview, placement };
+      : { username, referral, interview, placement };
 
   return {
     statusCode: 200,
